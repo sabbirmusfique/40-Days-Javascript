2a8cd5e443fa692a1d50beabe6f52fb9dbd9a951
sabbirmusfique <sabbir.musfique@bedatasolutions.com>
2025-11-16 19:28:36 +0600

    day 2 completed

---
+++ Day-02/script.js
@@ -0,0 +1,38 @@
+//assign and declare variables
+let Sname="Sabbir";
+let age=24;
+let isStudent = false;
+let favoriteLanguage = "Javascript";
+
+console.log(Sname)
+console.log(age)
+console.log(isStudent)
+console.log(favoriteLanguage)
+
+//re assign variables value
+Sname = "musfique";
+age = 12;
+isStudent = true;
+favoriteLanguage = "C++";
+
+console.log(Sname)
+console.log(age)
+console.log(isStudent)
+console.log(favoriteLanguage)
+
+//Object
+const person = {
+    id: "1234",
+    Name: "Sabbir Musfique",
+    age: 25,
+    sex: "Male",
+};
+
+console.log("Person info", person)
+console.log(person.Name, " ", person.age, " ", person.sex)
+
+
+//Array
+const fruits = ["apple", "banana", "body"]
+console.log("array value : ", fruits[0], ", ", fruits[1])
+

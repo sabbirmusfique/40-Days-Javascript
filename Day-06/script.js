c3c34ecf577073b4da9b5de00794a817075c0d6f
sabbirmusfique <sabbir.musfique@bedatasolutions.com>
2025-11-18 00:01:50 +0600

    day 6 done

--- Day-06/script.js
+++ Day-06/script.js
@@ -1,121 +1,131 @@
-console.log("Day 04 Javascript")
+console.log("Day 06 Javascript")
 
-
-// 1. What will be the output of this code snippet and why?
-let day = "Monday";
-
-switch (day) {
-   case "monday":
-       console.log("It's the start of the week.");
-       break;
-   default:
-       console.log("It's a normal day.");
-}
-//output : It's a normal day.
-// reason : monday and Monday isn't same, Case sensative
-
-
-// 2. Build an ATM Cash Withdrawal System
-let amount = 1300;
-
-if(amount % 100 == 0){
-    console.log('Withdrawal successful')
-}
-else{
-    console.log('Invalid amount')
+//1. Write a Function to Convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celcius){
+  let ans = 32 + (9*celcius)/5;
+  return ans;
 }
 
-// 3. Build a Calculator with switch-case
-let a=6, b=3;
-let ch='/';
-switch (ch) {
-    case '+':
-        console.log(`Sum of ${a} and ${b} is :`, a+b)
-        break;
-    case '-':
-        console.log(`Sub of ${a} and ${b} is :`, a-b)
-        break;
-    case '*':
-        console.log(`Multiply of ${a} and ${b} is :`, a*b)
-        break;
-    case '/':
-        if(b == 0){
-            console.log("denominator can't be 0"); break;
-        }
-        console.log(`Div of ${a} and ${b} is :`, a/b)
-        break;
-    default:
-        console.log('Invalid operator')
-        break;
+let celcius = 35.5;
+console.log(`${celcius} Celcius = ${celsiusToFahrenheit(celcius)} Fareheit`)
+
+//2. Create a Function to Find the Maximum of Two Numbers
+function findMax(num1, num2){
+  if(num1 >= num2) return num1;
+  return num2;
 }
 
-// 4. Pay for your movie ticket
-let age = 25;
-if(age < 18){
-    console.log("Ticket price : $3")
-}
-else if(age >= 18 && age <= 60){
-    console.log("Ticket price : $10")
-}
-else{
-    console.log("Ticket price : $8")
+let num1=-123, num2=320;
+console.log(`Max of ${num1} and ${num2} is : ${findMax(num1, num2)}`)
+
+//3. Function to Check if a String is a Palindrome
+function isPalindrome(str) {
+  let n = str.length;
+  for(let i=0; i<n/2; i++){
+    if(str[i] != str[n-i-1]) return false;
+  }
+
+  return true;
 }
 
-// 5. Horoscope Sign Checker
-let birthMonth = "February";
-let zodiacSign = "";
-switch (birthMonth.toLowerCase()) {
-  case "january":
-    zodiacSign = "Capricorn";
-    break;
-  case "february":
-    zodiacSign = "Aquarius";
-    break;
-  case "march":
-    zodiacSign = "Pisces";
-    break;
-  case "april":
-    zodiacSign = "Aries";
-    break;
-  case "may":
-    zodiacSign = "Taurus";
-    break;
-  case "june":
-    zodiacSign = "Gemini";
-    break;
-  case "july":
-    zodiacSign = "Cancer";
-    break;
-  case "august":
-    zodiacSign = "Leo";
-    break;
-  case "september":
-    zodiacSign = "Virgo";
-    break;
-  case "october":
-    zodiacSign = "Libra";
-    break;
-  case "november":
-    zodiacSign = "Scorpio";
-    break;
-  case "december":
-    zodiacSign = "Sagittarius";
-    break;
-  default:
-    zodiacSign = "Invalid month";
+let st = "madam";
+if(isPalindrome(st)) console.log(`${st} is a Palindrome`)
+else console.log(`${st} is not a Palindrome`)
+
+//4. Write a Function to Find Factorial of a Number
+function factorial(n){
+  let ans=1;
+  for( let i=1; i<=n; i++){
+    ans *= i;
+  }
+  return ans;
 }
 
-console.log(`Month: ${birthMonth}, Sign: ${zodiacSign}`);
+let fact = 5;
+console.log(`Factorial of ${fact} is : ${factorial(fact)}`)
 
-//6. Which Triangle?
-let aa=20, bb=20, cc=20;
+//5. Write a function to Count Vowels in a String
+function countVowels(str){
+  let count = 0;
+  str = str.toLowerCase();
+  for(let i=0; i<str.length; i++){
+    if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') count++;
+  }
 
-if(aa === bb && bb === cc){
-    console.log("Equilateral Triangle")
+  return count;
 }
-else if(aa !== bb && bb !== cc && cc !== aa){
-    console.log("Scalene Triangle")
+
+let vow = "Mohammad Sabbir Musfique";
+console.log(`Vowel counts in '${vow}' is : ${countVowels(vow)}`)
+
+//6. Write a Function to Capitalize the First Letter of Each Word in a Sentence
+function capitalizeWords(sentence){
+  let str = sentence.toUpperCase();
+  return str;
 }
-else{
-    console.log("Isosceles Triangle")
+
+let word = "Hello World Sabbir"
+console.log(`Capitalize sentence is '${capitalizeWords(word)}.'`);
+
+//7. Use an IIFE to Print “Hello, JavaScript!”
+(function (str){
+  console.log("Hello, " + str);
+})('JavaScript');
+
+//8. Create a Simple Callback Function
+function greet(name, callback){
+  console.log("Outer function called")
+  callback(name);
 }
+
+function callback(name){
+  console.log(`Greetings ${name} from callback function`)
+}
+
+greet("Sabbir", callback)
+
+//9. Create Call Stack Execution Diagram for this flow
+/*
+
+function f1() {}
+function f2() {
+    f1();
+}
+f2(); 
+
+--------
+Call seq : 
+f2 called
+f1 called
+f1 executed done
+f2 executed done
+
+*/
+
+
+
+//10. Create Call Stack Execution Diagram for this flow
+/*
+
+function f1() {}
+function f2() {}
+function f3() {
+    f1();
+}
+f2();
+f3();
+f1(); 
+
+--------
+Call seq : 
+f2 called
+f2 execution done
+
+f3 called
+f1 called
+f1 executed done
+f3 executed done
+
+f1 called
+f1 executed done
+*/
